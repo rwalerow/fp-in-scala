02b336671eefea921ffb7b01219dceddd06d7826
Robert <viniolli@gmail.com>
2017-04-09 11:32:25 +0200

    Move tree test to correct file

--- src/test/java/rwalerow/chapter3/TreeSpec.scala
+++ src/test/java/rwalerow/chapter3/TreeSpec.scala
@@ -2,7 +2,7 @@ package rwalerow.chapter3
 
 import org.scalatest.{Matchers, WordSpec}
 import rwalerow.chapter3.Exercieses.maximum
-import rwalerow.chapter3.Tree.size2
+import rwalerow.chapter3.Tree.{depth, depth2, size2}
 
 class TreeSpec extends WordSpec with Matchers {
 
@@ -38,4 +38,87 @@ class TreeSpec extends WordSpec with Matchers {
     }
   }
 
+  "Tree depth" should {
+    "calculate one for leaf" in {
+      val t = Leaf(1)
+
+      depth(t) shouldBe 1
+      depth2(t) shouldBe 1
+    }
+
+    "calculate small tree heigth" in {
+      val t = Branch(
+        Branch(Leaf(1), Leaf(2)),
+        Leaf(10)
+      )
+
+      depth(t) shouldBe 3
+      depth2(t) shouldBe 3
+    }
+
+    "calculate bigger tree" in {
+      val t = Branch(
+        Branch(
+          Branch(
+            Branch(
+              Leaf(1),
+              Branch(
+                Branch(Leaf(3), Leaf(35)),
+                Leaf(21)
+              )
+            ),
+            Leaf(2)
+          ),
+          Branch(Leaf(324), Leaf(12))
+        ),
+        Branch(Leaf(2), Leaf(13))
+      )
+
+      depth(t) shouldBe 7
+      depth2(t) shouldBe 7
+    }
+  }
+
+  "tree Map" should {
+    "Add 1 to all nodes" in {
+      val t = Branch(
+        Leaf(1), Leaf(4)
+      )
+
+      val expected = Branch(
+        Leaf(2), Leaf(5)
+      )
+
+      Tree.map(t)(_ + 1) shouldBe expected
+      Tree.map2(t)(_ + 1) shouldBe expected
+    }
+
+    "Change bigger tree" in {
+      val t = Branch(
+        Branch(
+          Branch(
+            Leaf(1),
+            Leaf(2)
+          ),
+          Leaf(3)
+        ),
+        Leaf(4)
+      )
+
+      val expected = Branch(
+        Branch(
+          Branch(
+            Leaf("2"),
+            Leaf("3")
+          ),
+          Leaf("4")
+        ),
+        Leaf("5")
+      )
+
+      val f: Int => String = ((x: Int) => x.toString).compose((x: Int) => x + 1)
+      Tree.map(t)(f) shouldBe expected
+      Tree.map2(t)(f) shouldBe expected
+    }
+  }
 }
